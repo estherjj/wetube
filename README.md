80d06888a2224d8679e5abba4a43a2060ca2bdfd
esther jeon <wjswls109@naver.com>
2021-05-09 01:28:06 +0900

    #views pug파일 수정완료
    -views pug파일 각 화면에 맞게 태그 등 수정완료

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # Wetube
 
 Cloning Youtube with VanillaJS and NodeJS
+
+## Page:
+
+- [ ] Home
+- [x] Join
+- [x] Login
+- [x] Search
+- [ ] User Detail
+- [x] Edit Profile
+- [x] Change Password
+- [x] Upload
+- [ ] Video Detail
+- [x] Edit Video
--- controllers/videoConstroller.js
+++ controllers/videoConstroller.js
@@ -9,8 +9,8 @@ export const videos = (req, res) => res.render("videos", {pageTitle: "Videos"});
 
 export const upload = (req, res) => res.render("upload", {pageTitle: "Upload"});
 
-export const videoDetail = (req, res) => res.render("video-Detail", {pageTitle: "Video Detail"});
+export const videoDetail = (req, res) => res.render("videoDetail", {pageTitle: "Video Detail"});
 
-export const editVideo = (req, res) => res.render("edit-Video", {pageTitle: "Edit Video"});
+export const editVideo = (req, res) => res.render("editVideo", {pageTitle: "Edit Video"});
 
-export const deleteVideo = (req, res) => res.render("delete-Video", {pageTitle: "Delete Video"});
+export const deleteVideo = (req, res) => res.render("deleteVideo", {pageTitle: "Delete Video"});
